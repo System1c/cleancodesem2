df77ec830d61962f627ab462a8d7a2dac1c2ed74
System1c <dinerapremaratne@gmail.com>
2022-10-11 04:17:56 +0530

    table for viewing of data

--- src/main/java/com/apiit/assignment/client/interfaces/UI/PrimenumclassGUI.java
+++ src/main/java/com/apiit/assignment/client/interfaces/UI/PrimenumclassGUI.java
@@ -3,6 +3,7 @@ package com.apiit.assignment.client.interfaces.UI;
 import com.apiit.assignment.client.interfaces.controllers.PrimenumGUIcontroller;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +12,8 @@ public class PrimenumclassGUI implements ActionListener{
     static JTextField primeField;
     static JLabel result;
     private static JLabel time;
+    static JTable table;
+    static DefaultTableModel model;
 
     static FrameGUI frameGUI = new FrameGUI();
 
@@ -39,6 +42,16 @@ public class PrimenumclassGUI implements ActionListener{
         time.setBounds(110, 150, 160, 25);
         primepanel.add(time);
 
+        String column[]={"Number","Result"};
+
+
+        model = new DefaultTableModel(column, 0);
+        table = new JTable(model);
+        table.setEnabled(false);
+        JScrollPane sp=new JScrollPane(table);
+        sp.setBounds(100,175,160,150);
+        primepanel.add(sp);
+
         primeframe.setVisible(true);
     }
 
@@ -49,7 +62,14 @@ public class PrimenumclassGUI implements ActionListener{
         pnfc.text = primeField.getText();
         String res = pnfc.primenumintcontroller();
         String ttime = pnfc.timecalc();
+
+        model.addRow(new Object[]{pnfc.text, res});
+
         result.setText("Nth Prime is " + res);
         time.setText(ttime);
+
+
+
     }
+
 }
--- src/main/java/com/apiit/assignment/client/interfaces/UI/WordcountGUI.java
+++ src/main/java/com/apiit/assignment/client/interfaces/UI/WordcountGUI.java
@@ -3,6 +3,7 @@ package com.apiit.assignment.client.interfaces.UI;
 import com.apiit.assignment.client.interfaces.controllers.WordcountGUIcontroller;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +14,8 @@ public class WordcountGUI implements ActionListener{
     private static JLabel wordResult;
     private static JLabel wordTime;
 
+    static JTable table;
+    static DefaultTableModel model;
     static FrameGUI frameGUI = new FrameGUI();
 
     public static void main() {
@@ -38,6 +41,17 @@ public class WordcountGUI implements ActionListener{
         wordTime.setBounds(110, 150, 160, 25);
         countpanel.add(wordTime);
 
+        String column[]={"Path","Word Count"};
+
+
+        model = new DefaultTableModel(column, 0);
+        table = new JTable(model);
+        table.setEnabled(false);
+        JScrollPane sp=new JScrollPane(table);
+        sp.setBounds(100,175,160,150);
+        countpanel.add(sp);
+
+
         countframe.setVisible(true);
     }
 
@@ -47,6 +61,7 @@ public class WordcountGUI implements ActionListener{
         wcc.path =  wordField.getText();
         String res = wcc.bookpathcalc();
         String ttime = wcc.booktimecalc();
+        model.addRow(new Object[]{wcc.path, res});
         wordResult.setText(res);
         wordTime.setText(ttime);
     }
